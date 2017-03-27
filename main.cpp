a1c56b47d56fbbe4166dde8f915dd80971ccc56c
Ivan Chapkailo <chapkailo.ivan@gmail.com>
2017-03-27 17:45:39 +0300

    Added geometry

--- main.cpp
+++ main.cpp
@@ -40,7 +40,25 @@ int WINAPI WinMain(HINSTANCE hInstance,
 // by CHAPKAILO IVAN
 //--------------------------------------------------------------------------------------
 
+//--------------------------------------------------------------------------------------
+// Structures
+// Структури
+//--------------------------------------------------------------------------------------
+struct SimpleVertex
+{
+	XMFLOAT3 Pos;
+	XMFLOAT4 Color;
+};
 
+
+struct ConstantBuffer
+{
+	XMMATRIX mWorld;
+	//XMMATRIX mWorldY;
+	//XMMATRIX mWorldZ;
+	XMMATRIX mView;
+	XMMATRIX mProjection;
+};
 
 //--------------------------------------------------------------------------------------
 // global variables
@@ -56,6 +74,19 @@ ID3D11Device*           g_pd3dDevice = NULL; // головний об'єкт, через який ми б
 ID3D11DeviceContext*    g_pImmediateContext = NULL; //другий головний успадкований об'єкт / Second main inherited object
 IDXGISwapChain*         g_pSwapChain = NULL; // об'єкт для ланцюжка-обміну / Object for chain-swap
 ID3D11RenderTargetView* g_pRenderTargetView = NULL; //поверхня для рендеринга / surface for rendering
+ID3D11Texture2D*        g_pDepthStencil = NULL;
+ID3D11DepthStencilView* g_pDepthStencilView = NULL;
+ID3D11VertexShader*     g_pVertexShader = NULL;
+ID3D11PixelShader*      g_pPixelShader = NULL;
+ID3D11InputLayout*      g_pVertexLayout = NULL;
+ID3D11Buffer*           g_pVertexBuffer = NULL;
+ID3D11Buffer*           g_pIndexBuffer = NULL;
+ID3D11Buffer*           g_pConstantBuffer = NULL;
+XMMATRIX                g_World;
+//XMMATRIX                g_WorldY;
+//XMMATRIX                g_WorldZ;
+XMMATRIX                g_View;
+XMMATRIX                g_Projection;
 
 
 //--------------------------------------------------------------------------------------
@@ -64,6 +95,7 @@ ID3D11RenderTargetView* g_pRenderTargetView = NULL; //поверхня для рендеринга / 
 //--------------------------------------------------------------------------------------
 HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow); // creatinf window function
 HRESULT InitDevice();
+HRESULT InitGeometry();
 void CleanupDevice();
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 void Render();
@@ -84,6 +116,10 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 	if (FAILED(InitDevice()))
 	{
 		CleanupDevice();
+		return 0;
+	}
+	if (FAILED(InitGeometry()))
+	{
 		return 0;
 	}
 
@@ -255,6 +291,38 @@ HRESULT InitDevice()
 	if (FAILED(hr))
 		return hr;
 
+	// Create a surface for Z-buffer
+	// Створення поверхні для Z-буфера
+	D3D11_TEXTURE2D_DESC descDepth;
+	ZeroMemory(&descDepth, sizeof(descDepth));
+	descDepth.Width = width;
+	descDepth.Height = height;
+	descDepth.MipLevels = 1;
+	descDepth.ArraySize = 1;
+	descDepth.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	descDepth.SampleDesc.Count = 1;
+	descDepth.SampleDesc.Quality = 0;
+	descDepth.Usage = D3D11_USAGE_DEFAULT;
+	descDepth.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	descDepth.CPUAccessFlags = 0;
+	descDepth.MiscFlags = 0;
+	hr = g_pd3dDevice->CreateTexture2D(&descDepth, NULL, &g_pDepthStencil);
+	if (FAILED(hr))
+		return hr;
+
+	// Creating a z-buffer
+	// Створення z-буфера
+	D3D11_DEPTH_STENCIL_VIEW_DESC descDSV;
+	ZeroMemory(&descDSV, sizeof(descDSV));
+	descDSV.Format = descDepth.Format;
+	descDSV.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	descDSV.Texture2D.MipSlice = 0;
+	hr = g_pd3dDevice->CreateDepthStencilView(g_pDepthStencil, &descDSV, &g_pDepthStencilView);
+	if (FAILED(hr))
+		return hr;
+
+	g_pImmediateContext->OMSetRenderTargets(1, &g_pRenderTargetView, g_pDepthStencilView);
+
 	// Specify Direct3D that this target renderer should be used by default and that it is up to him to draw everything
 	// Вказуємо Direct3D, що цей рендер-таргет треба використовувати за замовчуванням і що саме в нього і треба все малювати
 	g_pImmediateContext->OMSetRenderTargets(1, &g_pRenderTargetView, 0);
@@ -273,6 +341,170 @@ HRESULT InitDevice()
 	return S_OK;
 }
 
+//--------------------------------------------------------------------------------------
+// Initializing geometry
+// ініціалізація геометрії
+//--------------------------------------------------------------------------------------
+HRESULT InitGeometry()
+{
+	RECT rc;
+	GetClientRect(g_hWnd, &rc);
+	UINT width = rc.right - rc.left;
+	UINT height = rc.bottom - rc.top;
+
+	// Downloading shaders
+	// завантажуємо шейдери
+	ID3DBlob* pVSBlob = NULL;
+	HRESULT hr;
+	hr = CompileShaderFromFile(L"Lesson2.fx", "VS", "vs_4_0", &pVSBlob);
+	if (FAILED(hr))
+	{
+		MessageBox(NULL,
+			L"The FX file cannot be compiled.  Please run this executable from the directory that contains the FX file.", L"Error", MB_OK);
+		return hr;
+	}
+
+	// Vertex Shader
+	// вершинний шейдер
+	hr = g_pd3dDevice->CreateVertexShader(pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), NULL, &g_pVertexShader);
+	if (FAILED(hr))
+	{
+		pVSBlob->Release();
+		return hr;
+	}
+
+	// Determining the format of the vertex buffer
+	// Визначення формату вершинного буфера
+	D3D11_INPUT_ELEMENT_DESC layout[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	};
+	UINT numElements = ARRAYSIZE(layout);
+
+	// Creating a buffer format
+	// Створення формату буфера
+	hr = g_pd3dDevice->CreateInputLayout(layout, numElements, pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), &g_pVertexLayout);
+	pVSBlob->Release();
+	if (FAILED(hr))
+		return hr;
+
+	// Setting the buffer format
+	// Установка формата буфера
+	g_pImmediateContext->IASetInputLayout(g_pVertexLayout);
+
+	// Pixel shader
+	// Піксельний шейдер
+	ID3DBlob* pPSBlob = NULL;
+	hr = CompileShaderFromFile(L"Lesson2.fx", "PS", "ps_4_0", &pPSBlob);
+	if (FAILED(hr))
+	{
+		MessageBox(NULL,
+			L"The FX file cannot be compiled.  Please run this executable from the directory that contains the FX file.", L"Error", MB_OK);
+		return hr;
+	}
+
+	// Pixel shader
+	// Піксельний шейдер
+	hr = g_pd3dDevice->CreatePixelShader(pPSBlob->GetBufferPointer(), pPSBlob->GetBufferSize(), NULL, &g_pPixelShader);
+	pPSBlob->Release();
+	if (FAILED(hr))
+		return hr;
+
+	// Creation of geometry for the vertex buffer
+	// Створення геометрії для вершинного буфера
+	SimpleVertex vertices[] =
+	{
+		{ XMFLOAT3(-1.0f, 1.0f, -1.0f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f) },
+		{ XMFLOAT3(1.0f, 1.0f, -1.0f), XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f) },
+		{ XMFLOAT3(1.0f, 1.0f, 1.0f), XMFLOAT4(0.0f, 1.0f, 1.0f, 1.0f) },
+		{ XMFLOAT3(-1.0f, 1.0f, 1.0f), XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f) },
+		{ XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT4(1.0f, 0.0f, 1.0f, 1.0f) },
+		{ XMFLOAT3(1.0f, -1.0f, -1.0f), XMFLOAT4(1.0f, 1.0f, 0.0f, 1.0f) },
+		{ XMFLOAT3(1.0f, -1.0f, 1.0f), XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f) },
+		{ XMFLOAT3(-1.0f, -1.0f, 1.0f), XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f) },
+	};
+	D3D11_BUFFER_DESC bd;
+	ZeroMemory(&bd, sizeof(bd));
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(SimpleVertex) * 8;
+	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	bd.CPUAccessFlags = 0;
+	D3D11_SUBRESOURCE_DATA InitData;
+	ZeroMemory(&InitData, sizeof(InitData));
+	InitData.pSysMem = vertices;
+	hr = g_pd3dDevice->CreateBuffer(&bd, &InitData, &g_pVertexBuffer);
+	if (FAILED(hr))
+		return hr;
+
+	// Vertex Buffer Setting
+	// Установка вершинного буфера
+	UINT stride = sizeof(SimpleVertex);
+	UINT offset = 0;
+	g_pImmediateContext->IASetVertexBuffers(0, 1, &g_pVertexBuffer, &stride, &offset);
+
+	// Creating an index buffer
+	// Створення індексного буфера, щоб знати які вершини групуються в трикутник
+	WORD indices[] =
+	{
+		3,1,0,
+		2,1,3,
+
+		0,5,4,
+		1,5,0,
+
+		3,4,7,
+		0,4,3,
+
+		1,6,5,
+		2,6,1,
+
+		2,7,6,
+		3,7,2,
+
+		6,4,5,
+		7,4,6,
+	};
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(WORD) * 36;        // 36 vertices needed for 12 triangles in a triangle list
+	bd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	bd.CPUAccessFlags = 0;
+	InitData.pSysMem = indices;
+	hr = g_pd3dDevice->CreateBuffer(&bd, &InitData, &g_pIndexBuffer);
+	if (FAILED(hr))
+		return hr;
+
+	// Set the index buffer
+	// Установка індексного буфера
+	g_pImmediateContext->IASetIndexBuffer(g_pIndexBuffer, DXGI_FORMAT_R16_UINT, 0);
+
+	// Setting a primitive type
+	// Установка типу примітив
+	g_pImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	// Creating a Shader Constant Buffer
+	// Створення буфера констант шейдера
+	bd.Usage = D3D11_USAGE_DEFAULT;
+	bd.ByteWidth = sizeof(ConstantBuffer);
+	bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	bd.CPUAccessFlags = 0;
+	hr = g_pd3dDevice->CreateBuffer(&bd, NULL, &g_pConstantBuffer);
+	if (FAILED(hr))
+		return hr;
+
+	// Installation of matrices
+	// установка матриць
+	g_World = XMMatrixIdentity();
+	//g_WorldY = XMMatrixIdentity();
+	//g_WorldZ = XMMatrixIdentity();
+	XMVECTOR Eye = XMVectorSet(0.0f, 1.0f, -5.0f, 0.0f);
+	XMVECTOR At = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+	XMVECTOR Up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+	g_View = XMMatrixLookAtLH(Eye, At, Up);
+	g_Projection = XMMatrixPerspectiveFovLH(XM_PIDIV2, width / (FLOAT)height, 0.01f, 100.0f);
+
+	return S_OK;
+}
 
 //--------------------------------------------------------------------------------------
 // Clearing Variables
@@ -282,6 +514,12 @@ void CleanupDevice()
 {
 	if (g_pImmediateContext) g_pImmediateContext->ClearState();
 
+	if (g_pConstantBuffer) g_pConstantBuffer->Release();
+	if (g_pVertexBuffer) g_pVertexBuffer->Release();
+	if (g_pIndexBuffer) g_pIndexBuffer->Release();
+	if (g_pVertexLayout) g_pVertexLayout->Release();
+	if (g_pVertexShader) g_pVertexShader->Release();
+	if (g_pPixelShader) g_pPixelShader->Release();
 	if (g_pRenderTargetView) g_pRenderTargetView->Release();
 	if (g_pSwapChain) g_pSwapChain->Release();
 	if (g_pImmediateContext) g_pImmediateContext->Release();
@@ -323,10 +561,35 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 //--------------------------------------------------------------------------------------
 void Render()
 {
+	// Setting a transformation for a cube
+	// Установка трансформації для куба
+	g_World = XMMatrixRotationX(3.14159f / 6.0f);
+	//g_WorldY = XMMatrixRotationY(3.14159f / 6.0f);
+	//g_WorldZ = XMMatrixRotationZ(0.0f);
+	
 	// Clearing a render-TARGET
 	// Очищення рендер-таргета
 	float ClearColor[4] = { 0.6f, 0.3f, 0.0f, 0.0f }; // колір / color
 	g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView, ClearColor);
+	g_pImmediateContext->ClearDepthStencilView(g_pDepthStencilView, D3D11_CLEAR_DEPTH, 1.0f, 0);
+
+	// Setting shader constants
+	// Установка констант шейдера
+	ConstantBuffer cb;
+	cb.mWorld = XMMatrixTranspose(g_World);
+	//cb.mWorldY = XMMatrixTranspose(g_WorldY);
+	//cb.mWorldZ = XMMatrixTranspose(g_WorldZ);
+	cb.mView = XMMatrixTranspose(g_View);
+	cb.mProjection = XMMatrixTranspose(g_Projection);
+	g_pImmediateContext->UpdateSubresource(g_pConstantBuffer, 0, NULL, &cb, 0, 0);
+
+	// Render cube
+	// Рендер куба
+	g_pImmediateContext->VSSetShader(g_pVertexShader, NULL, 0);
+	g_pImmediateContext->VSSetConstantBuffers(0, 1, &g_pConstantBuffer);
+	g_pImmediateContext->PSSetShader(g_pPixelShader, NULL, 0);
+	g_pImmediateContext->DrawIndexed(36, 0, 0);       // 36 вершин утворюють 12 полігонів, по три вершини на полігон /
+													  // 36 vertices form 12 polygons, three vertices per polygon
 
 	// Displaying the contents of the render-target
 	// Вивід на екран вмісту рендер-таргета
