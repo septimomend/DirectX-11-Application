71e9b2f1fcfe9cb8bd159a131771c76888ce48e8
Ivan Chapkailo <chapkailo.ivan@gmail.com>
2017-03-27 12:51:32 +0300

    Fix DirectX dll errors

---
+++ main.cpp
@@ -0,0 +1,335 @@
+#include <windows.h> // include the basic windows header file
+#include <D3DX11.h>
+#include <D3D11.h>
+#include <d3dcompiler.h>
+#include <xnamath.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <functional>
+#include "Resource.h"
+
+/*
+//////////////////////////////////////////////////////////////////
+//HELLO WORLD
+//////////////////////////////////////////////////////////////////
+// the entry point for any Windows program
+int WINAPI WinMain(HINSTANCE hInstance,
+		HINSTANCE hPrevInstance,
+		LPSTR lpCmdLine,
+		int nShowCmd)
+{
+	// create a "Hello World" message box using MessageBox()
+	MessageBox(NULL,
+		L"Hello World!",
+		L"Just another Hello World program!",
+		MB_ICONEXCLAMATION | MB_OK);
+
+	// return 0 to Windows
+	return 0;
+}
+*/
+
+//--------------------------------------------------------------------------------------
+// File: main.cpp
+// Gameloft_Homework
+// Here is creating window and rendering
+// by CHAPKAILO IVAN
+//--------------------------------------------------------------------------------------
+
+
+
+//--------------------------------------------------------------------------------------
+// global variables
+// Глобальні змінні
+//--------------------------------------------------------------------------------------
+// змінні зберігання екземплярів вікна / storing copies variables of windows
+HINSTANCE               g_hInst = NULL;
+HWND                    g_hWnd = NULL;
+// Direct3D
+D3D_DRIVER_TYPE         g_driverType = D3D_DRIVER_TYPE_NULL;
+D3D_FEATURE_LEVEL       g_featureLevel = D3D_FEATURE_LEVEL_11_0;
+ID3D11Device*           g_pd3dDevice = NULL; // головний об'єкт, через який ми будемо звертатися до пристрою / The main object through which we will refer to the device
+ID3D11DeviceContext*    g_pImmediateContext = NULL; //другий головний успадкований об'єкт / Second main inherited object
+IDXGISwapChain*         g_pSwapChain = NULL; // об'єкт для ланцюжка-обміну / Object for chain-swap
+ID3D11RenderTargetView* g_pRenderTargetView = NULL; //поверхня для рендеринга / surface for rendering
+
+
+//--------------------------------------------------------------------------------------
+// functions announcement
+// оголошення функцій
+//--------------------------------------------------------------------------------------
+HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow); // creatinf window function
+HRESULT InitDevice();
+void CleanupDevice();
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+void Render();
+
+
+//--------------------------------------------------------------------------------------
+// The main function of the program. There have all initialization and then executed cycle message
+// Головна функція програми. Відбуваються всі ініціалізації, а потім виконується цикл обробки повідомлень
+//--------------------------------------------------------------------------------------
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	if (FAILED(InitWindow(hInstance, nCmdShow)))
+		return 0;
+
+	if (FAILED(InitDevice()))
+	{
+		CleanupDevice();
+		return 0;
+	}
+
+	// cycle message
+	// цикл обробки повідомлень
+	MSG msg = { 0 };
+	while (WM_QUIT != msg.message)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			Render();
+		}
+	}
+
+	CleanupDevice();
+
+	return (int)msg.wParam;
+}
+
+
+//--------------------------------------------------------------------------------------
+// creating window
+// Створення вікна
+//--------------------------------------------------------------------------------------
+HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow)
+{
+	// Register class
+	// Регістрація класа вікна
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, (LPCTSTR)IDI_TUTORIAL1);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = L"main";
+	wcex.hIconSm = LoadIcon(wcex.hInstance, (LPCTSTR)IDI_TUTORIAL1);
+	if (!RegisterClassEx(&wcex))
+		return E_FAIL;
+
+	// Create window
+	// Створення вікна
+	g_hInst = hInstance;
+	RECT rc = { 0, 0, 1366, 768 }; // розмір / size
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+	g_hWnd = CreateWindow(L"main", L"Lesson2", 
+		WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, hInstance, NULL);
+	if (!g_hWnd)
+		return E_FAIL;
+
+	ShowWindow(g_hWnd, nCmdShow);
+
+	return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Auxiliary function for DX11 shader compilation
+// Допоміжна функція для компілювання шейдерів DX11
+//--------------------------------------------------------------------------------------
+HRESULT CompileShaderFromFile(WCHAR* szFileName, LPCSTR szEntryPoint, LPCSTR szShaderModel, ID3DBlob** ppBlobOut)
+{
+	HRESULT hr = S_OK;
+
+	DWORD dwShaderFlags = D3DCOMPILE_ENABLE_STRICTNESS;
+#if defined( DEBUG ) || defined( _DEBUG )
+	// Set the D3DCOMPILE_DEBUG flag to embed debug information in the shaders.
+	// Setting this flag improves the shader debugging experience, but still allows 
+	// the shaders to be optimized and to run exactly the way they will run in 
+	// the release configuration of this program.
+	dwShaderFlags |= D3DCOMPILE_DEBUG;
+#endif
+
+	ID3DBlob* pErrorBlob;
+	hr = D3DX11CompileFromFile(szFileName, NULL, NULL, szEntryPoint, szShaderModel,
+		dwShaderFlags, 0, NULL, ppBlobOut, &pErrorBlob, NULL);
+	if (FAILED(hr))
+	{
+		if (pErrorBlob != NULL)
+			OutputDebugStringA((char*)pErrorBlob->GetBufferPointer());
+		if (pErrorBlob) pErrorBlob->Release();
+		return hr;
+	}
+	if (pErrorBlob) pErrorBlob->Release();
+
+	return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Initialize Direct3D device
+// Ініціалізація Direct3D пристрою
+//--------------------------------------------------------------------------------------
+HRESULT InitDevice()
+{
+	HRESULT hr = S_OK;
+
+	RECT rc;
+	GetClientRect(g_hWnd, &rc);
+	UINT width = rc.right - rc.left;
+	UINT height = rc.bottom - rc.top;
+
+	UINT createDeviceFlags = 0;
+#ifdef _DEBUG
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	D3D_DRIVER_TYPE driverTypes[] =
+	{
+		D3D_DRIVER_TYPE_HARDWARE,
+		D3D_DRIVER_TYPE_WARP,
+		D3D_DRIVER_TYPE_REFERENCE,
+	};
+	UINT numDriverTypes = ARRAYSIZE(driverTypes);
+
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+	};
+	UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+
+	DXGI_SWAP_CHAIN_DESC sd;
+	ZeroMemory(&sd, sizeof(sd));
+	// sd structure parameters
+	// параметри структури sd
+	sd.BufferCount = 1;
+	sd.BufferDesc.Width = width;
+	sd.BufferDesc.Height = height;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.OutputWindow = g_hWnd;
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+	sd.Windowed = TRUE;
+
+	for (UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; driverTypeIndex++)
+	{
+		g_driverType = driverTypes[driverTypeIndex];
+		hr = D3D11CreateDeviceAndSwapChain(NULL, g_driverType, NULL, createDeviceFlags, featureLevels, numFeatureLevels,
+			D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &g_featureLevel, &g_pImmediateContext);
+		if (SUCCEEDED(hr))
+			break;
+	}
+	if (FAILED(hr))
+		return hr;
+
+	// Creating a render-TARGET
+	// Створення рендер-таргета
+	ID3D11Texture2D* pBackBuffer = NULL;
+	hr = g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+	if (FAILED(hr))
+		return hr;
+
+	hr = g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_pRenderTargetView);
+	pBackBuffer->Release();
+	if (FAILED(hr))
+		return hr;
+
+	// Specify Direct3D that this target renderer should be used by default and that it is up to him to draw everything
+	// Вказуємо Direct3D, що цей рендер-таргет треба використовувати за замовчуванням і що саме в нього і треба все малювати
+	g_pImmediateContext->OMSetRenderTargets(1, &g_pRenderTargetView, 0);
+
+	// Setup the viewport
+	// Налаштування вікна перегляду
+	D3D11_VIEWPORT vp;
+	vp.Width = (FLOAT)width;
+	vp.Height = (FLOAT)height;
+	vp.MinDepth = 0.0f;
+	vp.MaxDepth = 1.0f;
+	vp.TopLeftX = 0;
+	vp.TopLeftY = 0;
+	g_pImmediateContext->RSSetViewports(1, &vp);
+
+	return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Clearing Variables
+// очищення змінних
+//--------------------------------------------------------------------------------------
+void CleanupDevice()
+{
+	if (g_pImmediateContext) g_pImmediateContext->ClearState();
+
+	if (g_pRenderTargetView) g_pRenderTargetView->Release();
+	if (g_pSwapChain) g_pSwapChain->Release();
+	if (g_pImmediateContext) g_pImmediateContext->Release();
+	if (g_pd3dDevice) g_pd3dDevice->Release();
+}
+
+
+//--------------------------------------------------------------------------------------
+// Windows Message Handling Procedure
+// Процедура обробки повідомлень Windows
+//--------------------------------------------------------------------------------------
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Render
+// Рендер
+//--------------------------------------------------------------------------------------
+void Render()
+{
+	// Clearing a render-TARGET
+	// Очищення рендер-таргета
+	float ClearColor[4] = { 0.6f, 0.3f, 0.0f, 0.0f }; // колір / color
+	g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView, ClearColor);
+
+	// Displaying the contents of the render-target
+	// Вивід на екран вмісту рендер-таргета
+	g_pSwapChain->Present(0, 0);
+}
+
