353b03e54ddbc422252f334dc68c75481f52a352
Ivan Chapkailo <chapkailo.ivan@gmail.com>
2017-03-27 10:38:20 +0300

    main functions announcement and initialization

---
+++ Gameloft_Homework/Gameloft_Homework/main.cpp
@@ -0,0 +1,322 @@
+#include <windows.h>    // include the basic windows header file
+
+/*
+//////////////////////////////////////////////////////////////////
+//HELLO WORLD
+//////////////////////////////////////////////////////////////////
+// the entry point for any Windows program
+int WINAPI WinMain(HINSTANCE hInstance,
+		HINSTANCE hPrevInstance,
+		LPSTR lpCmdLine,
+		int nShowCmd)
+{
+	// create a "Hello World" message box using MessageBox()
+	MessageBox(NULL,
+		L"Hello World!",
+		L"Just another Hello World program!",
+		MB_ICONEXCLAMATION | MB_OK);
+
+	// return 0 to Windows
+	return 0;
+}
+*/
+
+//--------------------------------------------------------------------------------------
+// File: main.cpp
+// Gameloft_Homework
+// Here is creating window and rendering
+// by CHAPKAILO IVAN
+//--------------------------------------------------------------------------------------
+#include <windows.h>
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dcompiler.h>
+#include <xnamath.h>
+#include "resource.h"
+
+
+//--------------------------------------------------------------------------------------
+// global variables
+// Ãëîáàëüí³ çì³íí³
+//--------------------------------------------------------------------------------------
+HINSTANCE               g_hInst = NULL;
+HWND                    g_hWnd = NULL;
+D3D_DRIVER_TYPE         g_driverType = D3D_DRIVER_TYPE_NULL;
+D3D_FEATURE_LEVEL       g_featureLevel = D3D_FEATURE_LEVEL_11_0;
+ID3D11Device*           g_pd3dDevice = NULL;
+ID3D11DeviceContext*    g_pImmediateContext = NULL;
+IDXGISwapChain*         g_pSwapChain = NULL;
+ID3D11RenderTargetView* g_pRenderTargetView = NULL;
+
+
+//--------------------------------------------------------------------------------------
+// functions announcement
+// îãîëîøåííÿ ôóíêö³é
+//--------------------------------------------------------------------------------------
+HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow); // creatinf window function
+HRESULT InitDevice();
+void CleanupDevice();
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+void Render();
+
+
+//--------------------------------------------------------------------------------------
+// The main function of the program. There have all initialization and then executed cycle message
+// Ãîëîâíà ôóíêö³ÿ ïðîãðàìè. Â³äáóâàþòüñÿ âñ³ ³í³ö³àë³çàö³¿, à ïîò³ì âèêîíóºòüñÿ öèêë îáðîáêè ïîâ³äîìëåíü
+//--------------------------------------------------------------------------------------
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	if (FAILED(InitWindow(hInstance, nCmdShow)))
+		return 0;
+
+	if (FAILED(InitDevice()))
+	{
+		CleanupDevice();
+		return 0;
+	}
+
+	// cycle message
+	// öèêë îáðîáêè ïîâ³äîìëåíü
+	MSG msg = { 0 };
+	while (WM_QUIT != msg.message)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			Render();
+		}
+	}
+
+	CleanupDevice();
+
+	return (int)msg.wParam;
+}
+
+
+//--------------------------------------------------------------------------------------
+// creating window
+// Ñòâîðåííÿ â³êíà
+//--------------------------------------------------------------------------------------
+HRESULT InitWindow(HINSTANCE hInstance, int nCmdShow)
+{
+	// Register class
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, (LPCTSTR)IDI_TUTORIAL1);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = L"Article1";
+	wcex.hIconSm = LoadIcon(wcex.hInstance, (LPCTSTR)IDI_TUTORIAL1);
+	if (!RegisterClassEx(&wcex))
+		return E_FAIL;
+
+	// Create window
+	g_hInst = hInstance;
+	RECT rc = { 0, 0, 533, 400 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+	g_hWnd = CreateWindow(L"Gameloft_Homework", L"Lesson2", WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, hInstance,
+		NULL);
+	if (!g_hWnd)
+		return E_FAIL;
+
+	ShowWindow(g_hWnd, nCmdShow);
+
+	return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Auxiliary function for DX11 shader compilation
+// Äîïîì³æíà ôóíêö³ÿ äëÿ êîìï³ëþâàííÿ øåéäåð³â DX11
+//--------------------------------------------------------------------------------------
+HRESULT CompileShaderFromFile(WCHAR* szFileName, LPCSTR szEntryPoint, LPCSTR szShaderModel, ID3DBlob** ppBlobOut)
+{
+	HRESULT hr = S_OK;
+
+	DWORD dwShaderFlags = D3DCOMPILE_ENABLE_STRICTNESS;
+#if defined( DEBUG ) || defined( _DEBUG )
+	// Set the D3DCOMPILE_DEBUG flag to embed debug information in the shaders.
+	// Setting this flag improves the shader debugging experience, but still allows 
+	// the shaders to be optimized and to run exactly the way they will run in 
+	// the release configuration of this program.
+	dwShaderFlags |= D3DCOMPILE_DEBUG;
+#endif
+
+	ID3DBlob* pErrorBlob;
+	hr = D3DX11CompileFromFile(szFileName, NULL, NULL, szEntryPoint, szShaderModel,
+		dwShaderFlags, 0, NULL, ppBlobOut, &pErrorBlob, NULL);
+	if (FAILED(hr))
+	{
+		if (pErrorBlob != NULL)
+			OutputDebugStringA((char*)pErrorBlob->GetBufferPointer());
+		if (pErrorBlob) pErrorBlob->Release();
+		return hr;
+	}
+	if (pErrorBlob) pErrorBlob->Release();
+
+	return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Initialize Direct3D device
+// ²í³ö³àë³çàö³ÿ Direct3D ïðèñòðîþ
+//--------------------------------------------------------------------------------------
+HRESULT InitDevice()
+{
+	HRESULT hr = S_OK;
+
+	RECT rc;
+	GetClientRect(g_hWnd, &rc);
+	UINT width = rc.right - rc.left;
+	UINT height = rc.bottom - rc.top;
+
+	UINT createDeviceFlags = 0;
+#ifdef _DEBUG
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+
+	D3D_DRIVER_TYPE driverTypes[] =
+	{
+		D3D_DRIVER_TYPE_HARDWARE,
+		D3D_DRIVER_TYPE_WARP,
+		D3D_DRIVER_TYPE_REFERENCE,
+	};
+	UINT numDriverTypes = ARRAYSIZE(driverTypes);
+
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+	};
+	UINT numFeatureLevels = ARRAYSIZE(featureLevels);
+
+	DXGI_SWAP_CHAIN_DESC sd;
+	ZeroMemory(&sd, sizeof(sd));
+	sd.BufferCount = 1;
+	sd.BufferDesc.Width = width;
+	sd.BufferDesc.Height = height;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.OutputWindow = g_hWnd;
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+	sd.Windowed = TRUE;
+
+	for (UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; driverTypeIndex++)
+	{
+		g_driverType = driverTypes[driverTypeIndex];
+		hr = D3D11CreateDeviceAndSwapChain(NULL, g_driverType, NULL, createDeviceFlags, featureLevels, numFeatureLevels,
+			D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &g_featureLevel, &g_pImmediateContext);
+		if (SUCCEEDED(hr))
+			break;
+	}
+	if (FAILED(hr))
+		return hr;
+
+	// Creating a render-TARGET
+	// Ñòâîðåííÿ ðåíäåð-òàðãåòà
+	ID3D11Texture2D* pBackBuffer = NULL;
+	hr = g_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+	if (FAILED(hr))
+		return hr;
+
+	hr = g_pd3dDevice->CreateRenderTargetView(pBackBuffer, NULL, &g_pRenderTargetView);
+	pBackBuffer->Release();
+	if (FAILED(hr))
+		return hr;
+
+
+	g_pImmediateContext->OMSetRenderTargets(1, &g_pRenderTargetView, 0);
+
+	// Setup the viewport
+	// Íàëàøòóâàííÿ â³êíà ïåðåãëÿäó
+	D3D11_VIEWPORT vp;
+	vp.Width = (FLOAT)width;
+	vp.Height = (FLOAT)height;
+	vp.MinDepth = 0.0f;
+	vp.MaxDepth = 1.0f;
+	vp.TopLeftX = 0;
+	vp.TopLeftY = 0;
+	g_pImmediateContext->RSSetViewports(1, &vp);
+
+	return S_OK;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Clearing Variables
+// î÷èùåííÿ çì³ííèõ
+//--------------------------------------------------------------------------------------
+void CleanupDevice()
+{
+	if (g_pImmediateContext) g_pImmediateContext->ClearState();
+
+	if (g_pRenderTargetView) g_pRenderTargetView->Release();
+	if (g_pSwapChain) g_pSwapChain->Release();
+	if (g_pImmediateContext) g_pImmediateContext->Release();
+	if (g_pd3dDevice) g_pd3dDevice->Release();
+}
+
+
+//--------------------------------------------------------------------------------------
+// Windows Message Handling Procedure
+// Ïðîöåäóðà îáðîáêè ïîâ³äîìëåíü Windows
+//--------------------------------------------------------------------------------------
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch (message)
+	{
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+
+//--------------------------------------------------------------------------------------
+// Render
+// Ðåíäåð
+//--------------------------------------------------------------------------------------
+void Render()
+{
+	// Clearing a render-TARGET
+	// Î÷èùåííÿ ðåíäåð-òàðãåòà
+	float ClearColor[4] = { 0.0f, 0.9f, 0.5f, 1.0f }; // öâåò
+	g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView, ClearColor);
+
+	// Displaying the contents of the render-target
+	// Âèâ³ä íà åêðàí âì³ñòó ðåíäåð-òàðãåòà
+	g_pSwapChain->Present(0, 0);
+}
+
